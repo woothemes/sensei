24dd9a87a694a859fb3a76f15b0dfa4bc669543f
Imran Hossain <imranh920@gmail.com>
2024-04-10 01:14:00 +0600

    Show only applicatble emails to users

--- includes/internal/emails/class-email-user-settings.php
+++ includes/internal/emails/class-email-user-settings.php
@@ -55,23 +55,35 @@ class Email_User_Settings {
 	 * @internal
 	 */
 	public function add_opt_in_out_setting_fields_in_user_profile_page() {
-		$all_emails = $this->repository->get_all( null, -1 );
+		$show_teacher_emails = current_user_can( 'manage_options' ) || \Sensei_Teacher::is_a_teacher( get_current_user_id() );
+		$all_emails          = $this->repository->get_all( $show_teacher_emails ? null : 'student', -1 );
+		$list_table_instance = new Email_List_Table( $this->repository );
+		$available_emails    = array_filter(
+			$all_emails->items,
+			function ( $email ) use ( $list_table_instance ) {
+				return 'publish' === $email->post_status
+					&& $list_table_instance->is_email_available( $email );
+			}
+		);
 
 		?>
-			<h3><?php esc_html_e( 'Sensei Emails', 'sensei-lms' ); ?></h3>
+			<h3><?php esc_html_e( 'Sensei Email Subscriptions', 'sensei-lms' ); ?></h3>
 
 			<table class="form-table">
 				<?php
-				foreach ( $all_emails->items as $email ) {
+				foreach ( $available_emails as $email ) {
+					if ( 'publish' !== $email->post_status ) {
+						continue;
+					}
 					?>
 						<tr>
 							<th scope="row">
 								<?php echo esc_html( get_post_meta( $email->ID, '_sensei_email_description', true ) ); ?>
 							</th>
 							<td>
-								<label for="admin_bar_front">
-									<input name="admin_bar_front" type="checkbox" id="admin_bar_front" value="1" checked="checked">
-									<?php echo esc_attr( $email->post_title ); ?>"><?php echo esc_html( $email->post_title ); ?>
+								<label for="<?php esc_attr( $email->ID ); ?>">
+									<input name="<?php esc_attr( $email->ID ); ?>" type="checkbox" id="<?php esc_attr( $email->ID ); ?>" value="1" checked="checked">
+									<?php echo esc_html( $email->post_title ); ?>
 								</label>
 							</td>
 						</tr>
