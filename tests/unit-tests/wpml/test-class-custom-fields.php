b701c982c0ad1fb3317e1a5ce85f26d40f0b426c
Dmitry Merkushin <merkushin@gmail.com>
2024-04-10 20:53:30 -0600

    Translate product ID if possible

--- tests/unit-tests/wpml/test-class-custom-fields.php
+++ tests/unit-tests/wpml/test-class-custom-fields.php
@@ -100,4 +100,28 @@ class Custom_Fields_Test extends \WP_UnitTestCase {
 
 		$this->assertSame( 4, $actual );
 	}
+
+	public function testUpdateCourseWooCommerceProductBeforeCopied_WhenCalled_ReturnsMatchingCourseForNewLesson() {
+		/* Arrange. */
+		$custom_fields = new Custom_Fields();
+
+		$language_code_filter = function () {
+			return 'a';
+		};
+		add_filter( 'wpml_element_language_code', $language_code_filter, 10, 0 );
+
+		$object_id_fitler = function () {
+			return 4;
+		};
+		add_filter( 'wpml_object_id', $object_id_fitler, 10, 0 );
+
+		/* Act. */
+		$actual = $custom_fields->update_course_woocommerce_product_before_copied( 1, 2, 3, '_course_woocommerce_product' );
+
+		/* Clean up & Assert. */
+		remove_filter( 'wpml_element_language_code', $language_code_filter );
+		remove_filter( 'wpml_object_id', $object_id_fitler );
+
+		$this->assertSame( 4, $actual );
+	}
 }
