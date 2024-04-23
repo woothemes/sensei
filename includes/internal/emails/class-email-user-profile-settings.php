d4bbe8184bd6442e36629c00764ba5228d998836
Dmitry Merkushin <merkushin@gmail.com>
2024-04-22 23:55:40 -0600

    Extract views for email settings in user profile

--- includes/internal/emails/class-email-user-profile-settings.php
+++ includes/internal/emails/class-email-user-profile-settings.php
@@ -112,42 +112,7 @@ class Email_User_Profile_Settings {
 			return;
 		}
 
-		?>
-		<h3><?php esc_html_e( 'Sensei Email', 'sensei-lms' ); ?></h3>
-
-		<table class="form-table">
-			<?php
-			foreach ( $user_emails as $type => $emails ) {
-				?>
-					<tr>
-						<th scope="row">
-							<?php
-							switch ( $type ) {
-								case 'student':
-									esc_html_e( 'Student Emails', 'sensei-lms' );
-									break;
-								case 'teacher':
-									esc_html_e( 'Teacher Emails', 'sensei-lms' );
-									break;
-								default:
-									esc_html_e( 'Other Emails', 'sensei-lms' );
-									break;
-							}
-							?>
-						</th>
-						<td>
-							<?php
-							foreach ( $emails as $identifier => $email ) {
-								$this->render_email_subscription_setting( $user->ID, $identifier, $email );
-							}
-							?>
-						</td>
-					</tr>
-				<?php
-			}
-			?>
-		</table>
-		<?php
+		include __DIR__ . '/views/profile-email-settings.php';
 	}
 
 	/**
@@ -163,14 +128,7 @@ class Email_User_Profile_Settings {
 		$is_unsubscribed = ! $this->subscription->is_subscribed( $user_id, $identifier );
 		$description     = (string) get_post_meta( $email->ID, '_sensei_email_description', true );
 
-		?>
-		<fieldset>
-			<label for="<?php echo esc_attr( $this->get_field_id( $identifier ) ); ?>">
-				<input name="sensei-email-subscriptions[]" type="checkbox" value="<?php echo esc_attr( $identifier ); ?>" <?php checked( false, $is_unsubscribed ); ?> id="<?php echo esc_attr( $this->get_field_id( $identifier ) ); ?>">
-				<?php echo esc_html( $description ); ?>
-			</label>
-		</fieldset>
-		<?php
+		include __DIR__ . '/views/profile-email-settings-subscription.php';
 	}
 
 	/**
---
+++ includes/internal/emails/views/profile-email-settings-subscription.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template used to render a single email subscription setting in the user profile page.
+ *
+ * @package sensei
+ * @since $$next-version$$
+ *
+ * @var string $identifier The email identifier.
+ * @var string $description The email description.
+ * @var bool   $is_unsubscribed Whether the user is unsubscribed from the email.
+ *
+ * phpcs:disable WordPress.WP.EnqueuedResources, VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
+ */
+?>
+<fieldset>
+	<label for="<?php echo esc_attr( $this->get_field_id( $identifier ) ); ?>">
+		<input name="sensei-email-subscriptions[]" type="checkbox" value="<?php echo esc_attr( $identifier ); ?>" <?php checked( false, $is_unsubscribed ); ?> id="<?php echo esc_attr( $this->get_field_id( $identifier ) ); ?>">
+		<?php echo esc_html( $description ); ?>
+	</label>
+</fieldset>
