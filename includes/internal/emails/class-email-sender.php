3f2764618517a1a0a0dbb239f1e83dbd2091dc48
Imran Hossain <imranh920@gmail.com>
2024-06-03 05:36:15 +0600

    Send replacement param in email hook

--- includes/internal/emails/class-email-sender.php
+++ includes/internal/emails/class-email-sender.php
@@ -125,7 +125,7 @@ class Email_Sender {
 			/*
 			 * For documentation of the filter check class-sensei-emails.php file.
 			 */
-			if ( apply_filters( 'sensei_send_emails', true, $recipient, $subject, $message, $email_name ) ) {
+			if ( apply_filters( 'sensei_send_emails', true, $recipient, $subject, $message, $email_name, $replacement ) ) {
 				wp_mail(
 					$recipient,
 					$subject,
@@ -229,17 +229,17 @@ class Email_Sender {
 	 *
 	 * @internal
 	 *
-	 * @param string $string The string.
-	 * @param array  $placeholders The placeholders.
+	 * @param string $template_body The string.
+	 * @param array  $placeholders  The placeholders.
 	 *
 	 * @return string
 	 */
-	private function replace_placeholders( string $string, array $placeholders ): string {
+	private function replace_placeholders( string $template_body, array $placeholders ): string {
 		foreach ( $placeholders as $placeholder => $value ) {
-			$string = str_replace( '[' . $placeholder . ']', $value, $string );
+			$template_body = str_replace( '[' . $placeholder . ']', $value, $template_body );
 		}
 
-		return $string;
+		return $template_body;
 	}
 
 	/**
@@ -322,7 +322,7 @@ class Email_Sender {
 	 *
 	 * @return array Headers.
 	 */
-	private function get_email_headers():array {
+	private function get_email_headers(): array {
 		$settings = $this->settings->get_settings();
 		$headers  = [
 			'Content-Type: text/html; charset=UTF-8',
