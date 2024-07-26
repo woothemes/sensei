d0bb904026dd2b6b0e02e6c6d80776240b83574a
Dmitry Merkushin <merkushin@gmail.com>
2024-07-25 23:06:10 -0600

    Don't render the block on the backend

--- includes/blocks/class-sensei-course-actions-block.php
+++ includes/blocks/class-sensei-course-actions-block.php
@@ -19,22 +19,8 @@ class Sensei_Course_Actions_Block {
 	public function __construct() {
 		Sensei_Blocks::register_sensei_block(
 			'sensei-lms/course-actions',
-			[
-				'render_callback' => [ $this, 'render_course_actions' ],
-			],
+			array(),
 			Sensei()->assets->src_path( 'blocks/course-actions-block/course-actions' )
 		);
-	}
-
-	/**
-	 * Renders the block as an empty string.
-	 *
-	 * @param array  $attributes The block attributes.
-	 * @param string $content    The block content.
-	 *
-	 * @return string The block HTML.
-	 */
-	public function render_course_actions( array $attributes, string $content ): string { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.FoundAfterLastUsed
-		return '';
 	}
 }
