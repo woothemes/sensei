9336e3cd4311756a286dd4644a72918ae156395b
Imran Hossain <imranh920@gmail.com>
2024-08-07 14:08:24 +0530

    Update description of student enroll course email

--- includes/internal/emails/class-email-seeder-data.php
+++ includes/internal/emails/class-email-seeder-data.php
@@ -70,7 +70,7 @@ class Email_Seeder_Data {
 			'student_starts_course'       => [
 				'types'       => [ 'teacher' ],
 				'subject'     => __( '[student:displayname] enrolled in [course:name]', 'sensei-lms' ),
-				'description' => __( 'Course Started', 'sensei-lms' ),
+				'description' => __( 'Course Enrolled', 'sensei-lms' ),
 				'content'     => '<!-- wp:pattern {"slug":"sensei-lms/student-starts-course"} /-->',
 			],
 			'student_completes_course'    => [
