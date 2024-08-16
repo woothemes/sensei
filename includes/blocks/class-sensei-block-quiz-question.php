14a08b7bb56a276dc68ddc8e991151da9c85bc31
Donna Peplinskie <donnapep@gmail.com>
2024-08-16 12:00:40 -0400

    Revert change to quizzes

--- includes/blocks/class-sensei-block-quiz-question.php
+++ includes/blocks/class-sensei-block-quiz-question.php
@@ -22,39 +22,18 @@ class Sensei_Block_Quiz_Question {
 		Sensei_Blocks::register_sensei_block(
 			'sensei-lms/quiz-question',
 			[
-				'render_callback' => '__return_empty_string',
+				'render_callback' => [ $this, 'render_quiz_question' ],
 			],
 			Sensei()->assets->src_path( 'blocks/quiz/question-block' )
-		);
-		Sensei_Blocks::register_sensei_block(
-			'sensei-lms/question-description',
-			[
-				'render_callback' => '__return_empty_string',
-			],
-			Sensei()->assets->src_path( 'blocks/quiz/question-description-block' )
-		);
-		Sensei_Blocks::register_sensei_block(
-			'sensei-lms/question-answers',
-			[
-				'render_callback' => '__return_empty_string',
-			],
-			Sensei()->assets->src_path( 'blocks/quiz/question-answers-block' )
 		);
 	}
 
 	/**
 	 * Renders the block as an empty string.
 	 *
-	 * @deprecated 4.24.2
-	 *
-	 * @param array  $attributes The block attributes.
-	 * @param string $content    The block content.
-	 *
 	 * @return string The block HTML.
 	 */
-	public function render_quiz_question( array $attributes, string $content ): string { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.FoundAfterLastUsed
-		_deprecated_function( __METHOD__, '4.24.2', '__return_empty_string' );
-
+	public function render_quiz_question(): string {
 		return '';
 	}
 }
