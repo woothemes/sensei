4482a5c8c23507ddb96ba800c0d0e0ad536fde3e
Donna Peplinskie <donnapep@gmail.com>
2024-08-19 12:10:40 -0400

    No `render_callback` for quiz description and question answer blocks

--- includes/blocks/class-sensei-block-quiz-question.php
+++ includes/blocks/class-sensei-block-quiz-question.php
@@ -22,9 +22,21 @@ class Sensei_Block_Quiz_Question {
 		Sensei_Blocks::register_sensei_block(
 			'sensei-lms/quiz-question',
 			[
-				'render_callback' => [ $this, 'render_quiz_question' ],
+				'render_callback' => '__return_empty_string',
 			],
 			Sensei()->assets->src_path( 'blocks/quiz/question-block' )
+		);
+
+		Sensei_Blocks::register_sensei_block(
+			'sensei-lms/question-description',
+			[],
+			Sensei()->assets->src_path( 'blocks/quiz/question-description-block' )
+		);
+
+		Sensei_Blocks::register_sensei_block(
+			'sensei-lms/question-answers',
+			[],
+			Sensei()->assets->src_path( 'blocks/quiz/question-answers-block' )
 		);
 	}
 
