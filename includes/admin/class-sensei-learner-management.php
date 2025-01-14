7f4e94a8d9c87d33d966935ca9fde5d694374157
Donna Peplinskie <donna.peplinskie@automattic.com>
2025-01-14 10:19:09 -0500

    Fix whitespace

--- includes/admin/class-sensei-learner-management.php
+++ includes/admin/class-sensei-learner-management.php
@@ -526,7 +526,7 @@ class Sensei_Learner_Management {
 		}
 
 		$formatted_date = gmdate( 'Y-m-d H:i:s', $date->getTimestamp() );
-		$updated    = (bool) update_comment_meta( $comment_id, 'start', $formatted_date, $date_started );
+		$updated        = (bool) update_comment_meta( $comment_id, 'start', $formatted_date, $date_started );
 
 		/**
 		 * Filter sensei_learners_learner_updated
